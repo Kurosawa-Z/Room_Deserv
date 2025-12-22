69bb9279a5d327e5956bffa587ffe6af27fb1c97
Kurosawa-Z <gianhiginofungo@gmail.com>
2025-12-22 23:36:51 +0800

    main

---
+++ web/src/App.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
+export default function App() {
+  return (
+    <>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+}
